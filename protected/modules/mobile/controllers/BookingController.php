8214c6906991bc5e5732910953336f7f97f16217
shumingsixin <shuming_sixin@163.com>
2016-01-07 16:55:48 +0800

    快速预约接口提取 删除quickbooking控制器

--- protected/modules/mobile/controllers/BookingController.php
+++ protected/modules/mobile/controllers/BookingController.php
@@ -84,31 +84,13 @@ class BookingController extends MobileController {
             } else {
                 $this->render('bookDoctorAndroid', array('model' => $form));
             }
-        } else {
-            // 快速预约
-            $form = new BookQuickForm();
-            $form->initModel();
-            $userId = $this->getCurrentUserId();
-            if (isset($userId)) {
-                $form->setUserId($userId);
-            }
-            if ($this->isUserAgentWeixin()) {
-                $form->user_agent = StatCode::USER_AGENT_WEIXIN;
-            } else {
-                $form->user_agent = StatCode::USER_AGENT_MOBILEWEB;
-            }
-            //@TEST:
-            //    $data = $this->testDataQuickBook();
-            //    $form->setAttributes($data, true);
-            //操作系统判断 返回不同的页面
-            if ($this->isUserAgentIOS()) {
-                $this->render('quickbook', array('model' => $form));
-            } else {
-                $this->render('createAjaxFileUpload', array('model' => $form));
-            }
         }
     }
 
+    /**
+     * 预约专家和科室预约
+     * @throws CException
+     */
     public function actionAjaxCreate() {
         $output = array('status' => 'no');
         if (isset($_POST['booking'])) {
@@ -127,18 +109,6 @@ class BookingController extends MobileController {
                 $form->setDoctorData();
                 $form->initModel();
                 $form->validate();
-            } else {
-                // 快速预约
-                $form = new BookQuickForm();
-                $form->setAttributes($values, true);
-                $form->initModel();
-                $form->validate();
-                //验证码校验
-                $authMgr = new AuthManager();
-                $authSmsVerify = $authMgr->verifyCodeForBooking($form->mobile, $form->verify_code, null);
-                if ($authSmsVerify->isValid() === false) {
-                    $form->addError('verify_code', $authSmsVerify->getError('code'));
-                }
             }
 
             //验证码校验
@@ -147,6 +117,115 @@ class BookingController extends MobileController {
 //            if ($authSmsVerify->isValid() === false) {
 //                $form->addError('verify_code', $authSmsVerify->getError('code'));
 //            }
+            try {
+                if ($form->hasErrors() === false) {
+                    $booking = new Booking();
+                    // 处理booking.user_id
+                    $userId = $this->getCurrentUserId();
+                    $bookingUser = null;
+                    if (isset($userId)) {
+                        $bookingUser = $userId;
+                        $user = $this->getCurrentUser();
+                        $form->mobile = $user->mobile;
+                    } else {
+                        $mobile = $form->mobile;
+                        $user = User::model()->getByUsernameAndRole($mobile, StatCode::USER_ROLE_PATIENT);
+                        if (isset($user)) {
+                            $bookingUser = $user->getId();
+                        } else {
+                            // create new user.
+                            $userMgr = new UserManager();
+                            $user = $userMgr->createUserPatient($mobile);
+                            if (isset($user)) {
+                                $bookingUser = $user->getId();
+                            }
+                        }
+                    }
+                    $booking->setAttributes($form->attributes, true);
+                    if ($this->isUserAgentWeixin()) {
+                        $booking->user_agent = StatCode::USER_AGENT_WEIXIN;
+                    } else {
+                        $booking->user_agent = StatCode::USER_AGENT_MOBILEWEB;
+                    }
+                    $booking->user_id = $bookingUser;
+                    if ($booking->save() === false) {
+                        $output['errors'] = $booking->getErrors();
+                        throw new CException('error saving data.');
+                    }
+                    //预约单保存成功  生成一张支付单
+                    $orderMgr = new OrderManager();
+                    $salesOrder = $orderMgr->createSalesOrder($booking);
+                    if ($salesOrder->hasErrors() === false) {
+                        $output['status'] = 'ok';
+                        $output['salesOrderRefNo'] = $salesOrder->getRefNo();
+                        $output['booking']['id'] = $booking->getId();
+                    } else {
+                        $output['errors'] = $salesOrder->getErrors();
+                        throw new CException('error saving data.');
+                    }
+                } else {
+                    $output['errors'] = $form->getErrors();
+                    throw new CException('error saving data.');
+                }
+            } catch (CException $cex) {
+                $output['status'] = 'no';
+            }
+        } else {
+            $output['error'] = 'missing parameters';
+        }
+        //$this->renderJsonOutput($output);
+        if (isset($_POST['plugin'])) {
+            echo CJSON::encode($output);
+            Yii::app()->end(200, true); //结束 返回200
+        } else {
+            $this->renderJsonOutput($output);
+        }
+    }
+
+    /**
+     * 快速预约
+     */
+    public function actionCreateQuickBooking() {
+        //$values = $_GET;
+        //$request = Yii::app()->request;
+        // 快速预约
+        $form = new BookQuickForm();
+        $form->initModel();
+        $userId = $this->getCurrentUserId();
+        if (isset($userId)) {
+            $form->setUserId($userId);
+        }
+        if ($this->isUserAgentWeixin()) {
+            $form->user_agent = StatCode::USER_AGENT_WEIXIN;
+        } else {
+            $form->user_agent = StatCode::USER_AGENT_MOBILEWEB;
+        }
+        //@TEST:
+        //    $data = $this->testDataQuickBook();
+        //    $form->setAttributes($data, true);
+        //操作系统判断 返回不同的页面
+        if ($this->isUserAgentIOS()) {
+            $this->render('quickbook', array('model' => $form));
+        } else {
+            $this->render('createAjaxFileUpload', array('model' => $form));
+        }
+    }
+
+    public function actionAjaxCreateQuickBooking() {
+        $output = array('status' => 'no');
+        if (isset($_POST['booking'])) {
+            $values = $_POST['booking'];
+            // 快速预约
+            $form = new BookQuickForm();
+            $form->setAttributes($values, true);
+            $form->initModel();
+            $form->validate();
+            //验证码校验
+            $authMgr = new AuthManager();
+            $authSmsVerify = $authMgr->verifyCodeForBooking($form->mobile, $form->verify_code, null);
+            if ($authSmsVerify->isValid() === false) {
+                $form->addError('verify_code', $authSmsVerify->getError('code'));
+            }
             try {
                 if ($form->hasErrors() === false) {
                     $booking = new Booking();
