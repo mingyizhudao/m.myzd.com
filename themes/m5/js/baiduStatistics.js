1f7c94aef8c70201e26f58f104d43801c657b9b4
liukui <kui.liu@mingyizhudao.com>
2016-04-14 16:43:57 +0800

    添加站长统计

--- themes/m5/js/baiduStatistics.js
+++
@@ -1,7 +0,0 @@
-var _hmt = _hmt || [];
-(function () {
-    var hm = document.createElement("script");
-    hm.src = "//hm.baidu.com/hm.js?f2827eaf72479c39863db9e3de2399d8";
-    var s = document.getElementsByTagName("script")[0];
-    s.parentNode.insertBefore(hm, s);
-})();
---
+++ themes/m5/js/statistics.js
@@ -0,0 +1,12 @@
+//百度统计
+var _hmt = _hmt || [];
+(function () {
+    var hm = document.createElement("script");
+    hm.src = "//hm.baidu.com/hm.js?f2827eaf72479c39863db9e3de2399d8";
+    var s = document.getElementsByTagName("script")[0];
+    s.parentNode.insertBefore(hm, s);
+})();
+
+//站长统计
+var cnzz_protocol = (("https:" == document.location.protocol) ? " https://" : " http://");
+document.write(unescape("%3Cspan id='cnzz_stat_icon_1258560752'%3E%3C/span%3E%3Cscript src='" + cnzz_protocol + "s11.cnzz.com/z_stat.php%3Fid%3D1258560752' type='text/javascript'%3E%3C/script%3E"));
