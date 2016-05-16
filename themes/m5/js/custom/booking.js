d49e15c0f585d52e264dd22cec4c25683ed79f44
liukui <kui.liu@mingyizhudao.com>
2016-05-16 20:24:12 +0800

    加密测试

--- themes/m5/js/custom/booking.js
+++ themes/m5/js/custom/booking.js
@@ -566,14 +566,14 @@ $(function () {
             disabledBtn(btnSubmit);
             //form插件的异步无刷新提交
             actionUrl = domForm.attr('data-actionurl');
-            //returnUrl = domForm.attr("data-url-return");
-            //alert("asdf");
-            //btnSubmit.button("disable");
             var formdata = domForm.serializeArray();
+            var dataArray = structure_formdata('booking', formdata);
+            var encryptContext = do_encrypt(dataArray);
+            var param = {param: encryptContext};
             domForm.ajaxSubmit({
                 type: 'post',
                 url: actionUrl,
-                data: formdata,
+                data: param,
                 success: function (data) {
                     console.log(data);
                     //图片上传
