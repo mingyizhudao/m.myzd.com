27a4b56f469407a01d0800102f212f170e980608
Administrator <Administrator@USER-20160321HB>
2016-05-30 11:22:39 +0800

    test33333333333

--- protected/modules/mobile/controllers/TestController.php
+++ protected/modules/mobile/controllers/TestController.php
@@ -6,6 +6,6 @@ class TestController extends MobileController {
      * 安卓获取病人病历七牛上传权限
      */
     public function actionAjaxTest() {
-        echo 'this is the test2222222222222222222222222！';exit;
+        echo 'this is the test333333333333333333333333333！';exit;
     }
 }
