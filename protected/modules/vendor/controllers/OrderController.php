f3e8f84b6f2f6b4491194279a7278f9ec260882b
hzc <314551195@qq.com>
2016-06-16 01:35:21 +0800

    优化

--- protected/modules/vendor/controllers/OrderController.php
+++ protected/modules/vendor/controllers/OrderController.php
@@ -3,6 +3,9 @@
 class OrderController extends MobileController {
 
     public function actionView($refNo) {
+        if(empty($refNo)){
+            $refNo = Yii::app()->request->getParam('refno');
+        }
         $apiSvc = new ApiViewSalesOrder($refNo);
         $output = $apiSvc->loadApiViewData();
         $this->render('view', array(
