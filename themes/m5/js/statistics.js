c4ab42ec9268289b9b6f9d50bccda61520ace4b2
liukui <kui.liu@mingyizhudao.com>
2016-07-12 12:07:27 +0800

    百度统计优化

--- themes/m5/js/statistics.js
+++ themes/m5/js/statistics.js
@@ -6,6 +6,8 @@ var _hmt = _hmt || [];
     var s = document.getElementsByTagName("script")[0];
     s.parentNode.insertBefore(hm, s);
 })();
+var _bdhmProtocol = (("https:" == document.location.protocol) ? " https://" : " http://");
+document.write(unescape("%3Cscript src='" + _bdhmProtocol + "hm.baidu.com/h.js%3F252f4ef9a606f2370f1195abe11eb465' type='text/javascript'%3E%3C/script%3E"));
 
 //站长统计
 var cnzz_protocol = (("https:" == document.location.protocol) ? " https://" : " http://");
