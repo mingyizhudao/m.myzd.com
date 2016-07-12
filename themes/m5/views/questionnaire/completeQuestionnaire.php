576fc28c44d78cf1070d72c3b490df3df88a330b
liukui <kui.liu@mingyizhudao.com>
2016-07-12 10:28:00 +0800

    提交成功文字优化

--- themes/m5/views/questionnaire/completeQuestionnaire.php
+++ themes/m5/views/questionnaire/completeQuestionnaire.php
@@ -14,12 +14,12 @@ $this->show_footer = false;
             感谢您的信任！
         </div>
         <div class="text-justify pl30 pr30 pt10 pb10 line-h18e">
-            名医助手会对您提交的资料进行审核。如有手术指征，我们会在12个小时内与您电话确认，并安排后续面诊服务。
+            名医助手会对您提交的资料进行审核。如有手术指征，我们会在工作时间的2小时内与您电话确认，并安排后续面诊服务。
         </div>
         <div class="text-center pt50 pb10">
             <img src="http://7xsq2z.com2.z0.glb.qiniucdn.com/146762537877061" class="w140p">
         </div>
-        <div class="text-center pad10 c-black2">
+        <div class="text-center pad10 c-black2 pb50">
             关注名医主刀微信公众号，查看预约进度。
         </div>
     </div>
