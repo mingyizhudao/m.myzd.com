65837c2751af9240c9dce865a1464464d1688482
today0309 <605816863@qq.com>
2016-08-05 16:08:29 +0800

    修改JS加载顺序

--- protected/modules/weixinpub/views/test/test001.php
+++ protected/modules/weixinpub/views/test/test001.php
@@ -8,31 +8,26 @@ $signPackage = $jssdk->GetSignPackage();
 <head>
     <meta charset="UTF-8">
     <title></title>
-</head>
-<body>
-    <h2>just test</h2>
-</body>
-<script src="http://res.wx.qq.com/open/js/jweixin-1.0.0.js"></script>
-<script>
+    <script>
   // 注意：所有的JS接口只能在公众号绑定的域名下调用，公众号开发者需要先登录微信公众平台进入“公众号设置”的“功能设置”里填写“JS接口安全域名”。 
   // 如果发现在 Android 不能分享自定义内容，请到官网下载最新的包覆盖安装，Android 自定义分享接口需升级至 6.0.2.58 版本及以上。
   // 完整 JS-SDK 文档地址：http://mp.weixin.qq.com/wiki/7/aaa137b55fb2e0456bf8dd9148dd613f.html
-wx.config({
-    debug: false,
-    appId: '<?php echo $signPackage["appId"];?>',
-    timestamp: <?php echo $signPackage["timestamp"];?>,
-    nonceStr: '<?php echo $signPackage["nonceStr"];?>',
-    signature: '<?php echo $signPackage["signature"];?>',
-    jsApiList: [
-        'checkJsApi',
-        'openLocation',
-        'getLocation',
-        'onMenuShareTimeline',
-        'onMenuShareAppMessage',
-        'hideAllNonBaseMenuItem',
-	'showMenuItems',
-    ]
-});
+    wx.config({
+        debug: false,
+        appId: '<?php echo $signPackage["appId"];?>',
+        timestamp: <?php echo $signPackage["timestamp"];?>,
+        nonceStr: '<?php echo $signPackage["nonceStr"];?>',
+        signature: '<?php echo $signPackage["signature"];?>',
+        jsApiList: [
+            'checkJsApi',
+            'openLocation',
+            'getLocation',
+            'onMenuShareTimeline',
+            'onMenuShareAppMessage',
+            'hideAllNonBaseMenuItem',
+            'showMenuItems',
+        ]
+    });
     wx.ready(function() {
 		
         wx.hideAllNonBaseMenuItem();
@@ -63,8 +58,11 @@ wx.config({
         });
 		
     });
-	
-
-		
 </script>
+</head>
+<body>
+    <h2>just test</h2>
+</body>
+<script src="http://res.wx.qq.com/open/js/jweixin-1.0.0.js"></script>
+
 </html>
