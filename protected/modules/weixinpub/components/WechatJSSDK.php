0455dcc60a7bcdbb176daa508bc1ea0fc5f21d71
today0309 <605816863@qq.com>
2016-08-05 15:26:08 +0800

    test

--- protected/modules/weixinpub/components/WechatJSSDK.php
+++ protected/modules/weixinpub/components/WechatJSSDK.php
@@ -4,44 +4,34 @@
  *
  * @author zhongtw
  */
-class WechatJSSDK extends WeixinpubController {
-    
-    private $app_id;
-    
-    private $jsapi_ticket;
-    
-    public function init() {
-
-        parent::init();
-        
-        $this->loadWechatAccount();
-        $this->app_id = $this->wechatAccount->getAppId();    
-        
-        $this->loadWechatBaseInfo();
-        $this->jsapi_ticket = $this->wechatBaseInfo->getJsapiTicket();
-        
-    }
+class WechatJSSDK {
     
     public function GetSignPackage(){
+        
+        $weixinpubId = Yii::app()->getModule('weixinpub')->weixinpubId;
+        $wechatAccount = new WechatAccount();
+        $app_id = $wechatAccount->getByPubId($weixinpubId);
+        $wechatBaseInfo = new WechatBaseInfo();
+        $jsapi_ticket = $wechatBaseInfo->getByPubId($weixinpubId);
+          
         $nonceStr = CommonConfig::createNonceStr(16);
         $timestamp = time();
         $url = "http://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]";
-        
         // 这里参数的顺序要按照 key 值 ASCII 码升序排序
-        $string = "jsapi_ticket=$this->jsapi_ticket&noncestr=$nonceStr&timestamp=$timestamp&url=$url";
+        $string = "jsapi_ticket=$jsapi_ticket&noncestr=$nonceStr&timestamp=$timestamp&url=$url";
         $signature = sha1($string);
 
         $signPackage = array(
-            "appId"     => $this->app_id,
+            "appId"     => $app_id,
             "nonceStr"  => $nonceStr,
             "timestamp" => $timestamp,
             "url"       => $url,
             "signature" => $signature,
             "rawString" => $string
         );
-        //ob_clean();
-        //var_dump($signPackage);
+        
         return $signPackage;
+        
     }
     
 }
