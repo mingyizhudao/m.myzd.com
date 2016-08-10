f1e6ead7dfbb0ffee987eca3853867f87db37b37
today0309 <605816863@qq.com>
2016-08-10 11:04:35 +0800

    增加测试文件

--- protected/modules/weixinpub/controllers/TestController.php
+++ protected/modules/weixinpub/controllers/TestController.php
@@ -7,15 +7,22 @@
 header("Content-type: text/html; charset=utf-8");
 class TestController extends WeixinpubController {
 	
-	public $layout = false;
+    public $layout = false;
     
     public function actionTest001(){
         $this->render('test001');
     }
     
-	public function actionTest002(){
+    public function actionTest002(){
         $weixinpub_id = Yii::app()->getModule('weixinpub')->weixinpubId;
         $wechatKeyWord = WechatKeyWord::model()->getAllByAttributes(array('weixinpub_id'=>$weixinpub_id));
         var_dump($wechatKeyWord);
     }
+    
+    public function actionTest003(){
+        $weixinpub_id = Yii::app()->getModule('weixinpub')->weixinpubId;
+        echo WechatBaseInfo::model()->getByAttributes(array('weixinpub_id'=>$weixinpub_id))->getAccessToken();
+        Yii::app()->end();
+    }
+    
 }
