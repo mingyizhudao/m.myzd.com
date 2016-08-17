2953dbc55f80aa667a40139f1de7725380b017ec
today0309 <605816863@qq.com>
2016-08-17 16:42:56 +0800

    增加网页授权功能

---
+++ protected/modules/weixinpub/components/WechatOauth2.php
@@ -0,0 +1,135 @@
+<?php
+/**
+ * 通过网页授权获取openid通用方法
+ *
+ * @author zhongtw
+ */
+class WechatOauth2 {
+    
+    
+    private $appId;
+    
+    private $appSecret;
+    
+    
+     function __construct($appId, $appSecret) {
+        $this->appId = $appId;
+        $this->appSecret = $appSecret;
+     }
+    
+    
+    /**
+     * 通过code获得openid
+     * @return type
+     */
+    public function GetOpenid(){
+        if (!isset($_GET['code'])){//请求中没有code,通过页面跳转获取code
+            $url = "https://open.weixin.qq.com/connect/oauth2/authorize?appid=%s&redirect_uri=%s&response_type=code&scope=snsapi_base&state=STATE#wechat_redirect";
+            $redirect_uri = urlencode('http://'.$_SERVER['HTTP_HOST'].$_SERVER['PHP_SELF'].$_SERVER['QUERY_STRING']);
+            $url = sprintf($url, $this->appId, $redirect_uri);           
+            Header("Location: $url");
+            Yii::app()->exit();
+        } else {//请求中有code，通过code获取openid
+            $code = $_GET['code'];
+            $data = $this->getOpenidFromMp($code);
+            $openid = $data['openid'];
+            return $openid;
+        }
+    }
+	
+	
+    /**
+     * get请求，获得返回的数据
+	 * 
+     */
+    private function httpGet($url){
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, $url);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        $output = curl_exec($ch);
+        $data = json_decode($output,true);
+        return $data;
+    }
+    
+    
+    /**
+     * 
+     * 构造获取code的url连接
+     * @param string $redirectUrl 微信服务器回跳的url，需要url编码
+     * 
+     * @return 返回构造好的url
+     */
+    private function __CreateOauthUrlForCode($redirectUrl){
+        $urlObj["appid"] = $this->appId;
+        $urlObj["redirect_uri"] = "$redirectUrl";
+        $urlObj["response_type"] = "code";
+        $urlObj["scope"] = "snsapi_base";
+        $urlObj["state"] = "STATE"."#wechat_redirect";
+        $bizString = $this->ToUrlParams($urlObj);
+        return "https://open.weixin.qq.com/connect/oauth2/authorize?".$bizString;
+    }
+    
+    
+    /**
+     * 
+     * 拼接签名字符串
+     * @param array $urlObj
+     * 
+     * @return 返回已经拼接好的字符串
+     */
+    private function ToUrlParams($urlObj){
+        $buff = "";
+        foreach ($urlObj as $k => $v){
+            if($k != "sign"){
+                $buff .= $k . "=" . $v . "&";
+            }
+        }
+        $buff = trim($buff, "&");
+        return $buff;
+    }
+    
+    
+    /**
+     * 
+     * 通过code从工作平台获取openid机器access_token
+     * @param string $code 微信跳转回来带上的code
+     * 
+     * @return openid
+     */
+    public function GetOpenidFromMp($code){
+        $url = $this->__CreateOauthUrlForOpenid($code);
+        //初始化curl
+        $ch = curl_init();
+        //设置超时
+        curl_setopt($ch, CURLOPT_TIMEOUT, $this->curl_timeout);
+        curl_setopt($ch, CURLOPT_URL, $url);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER,FALSE);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYHOST,FALSE);
+        curl_setopt($ch, CURLOPT_HEADER, FALSE);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
+        //运行curl，结果以jason形式返回
+        $res = curl_exec($ch);
+        curl_close($ch);
+        $data = json_decode($res,true);
+        return $data;
+    }
+    
+    
+    /**
+     * 
+     * 构造获取open和access_toke的url地址
+     * @param string $code，微信跳转带回的code
+     * 
+     * @return 请求的url
+     */
+    private function __CreateOauthUrlForOpenid($code){
+        $urlObj["appid"] = $this->appId;
+        $urlObj["secret"] = $this->appSecret;
+        $urlObj["code"] = $code;
+        $urlObj["grant_type"] = "authorization_code";
+        $bizString = $this->ToUrlParams($urlObj);
+        return "https://api.weixin.qq.com/sns/oauth2/access_token?".$bizString;
+    }
+    
+    
+}
---
+++ protected/modules/weixinpub/controllers/RedpackController.php
@@ -0,0 +1,44 @@
+<?php
+/**
+ * 现金红包页面
+ *
+ * @author Administrator
+ */
+header("Content-type: text/html; charset=utf-8");
+class RedpackController extends WeixinpubController {
+    
+    private $appId;
+    
+    private $appSecret;
+    
+    public function init() {
+        parent::init();
+        $this->loadWechatAccount();
+        $wechatAccount = $this->wechatAccount;
+        $this->appId = $wechatAccount->getAppId();    
+        $this->appSecret = $wechatAccount->getAppSecret();  
+    }
+    
+    /**
+     * 进入红包活动页面
+     * @param type $subscriptions_id    订阅号openid
+     */
+    public function actionActivepage($subscriptions_id) {
+        $output = new stdClass();
+        $mem = Yii::app()->cache;
+        if(!isset($mem->get('peopleNum'))){
+            $mem->set('peopleNum','0');//初始化参与活动人数
+        }
+        $peopleNum = $mem->get('peopleNum');//获取已经参与活动的人数
+        if($peopleNum >= 5){//活动人数达到5000
+            //$this->render("redpackPage",array('flag'=>'1'));
+            echo '活动参与人数达到限制';
+            Yii::app()->end();
+        }else{
+            $mem->set('peopleNum',$peopleNum + 1);//初始化参与活动人数
+            $wechatOauth2 = new WechatOauth2($this->appId, $this->appSecret);
+            echo $wechatOauth2->GetOpenid();
+        }
+        
+    }
+}
