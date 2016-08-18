d8393c59448db072d7ef6fa72c0f9f180728d7cd
hzc <314551195@qq.com>
2016-08-18 14:47:33 +0800

    city

--- protected/controllers/Api2Controller.php
+++ protected/controllers/Api2Controller.php
@@ -319,11 +319,11 @@ class Api2Controller extends Controller {
                 break;
             case 'city':
                 $values = $_GET;
-                if($api>=14){
-                    $city = new ApiViewOpenCity($values);
-                }else{
+//                if($api>=14){
+//                    $city = new ApiViewOpenCity($values);
+//                }else{
                     $city = new ApiViewAppCity($values);
-                }
+//                }
                 $output = $city->loadApiViewData();
                 break;
             case 'diseasename'://根据疾病名称获取疾病信息
