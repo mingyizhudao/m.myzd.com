40233609b2541ec374825e8f730726dbf68f7fb1
jiangyan <18556506973@163.com>
2016-08-29 11:06:50 +0800

    修改首页轮播图

--- themes/m5/views/home/index.php
+++ themes/m5/views/home/index.php
@@ -4,7 +4,7 @@ Yii::app()->clientScript->registerScriptFile('http://myzd.oss-cn-hangzhou.aliyun
 Yii::app()->clientScript->registerScriptFile('http://myzd.oss-cn-hangzhou.aliyuncs.com/static/mobile/js/jquery-1.9.1.min.js', CClientScript::POS_HEAD);
 ?>
 <style>
-   .y1{background:#ffc24b;} 
+    .y1{background:#ffc24b;} 
     .o1{background:#f1866e;} 
     .b1{background:#84bef0;} 
     .b2{background:#639deb;}
@@ -13,11 +13,10 @@ Yii::app()->clientScript->registerScriptFile('http://myzd.oss-cn-hangzhou.aliyun
     .b4{background:#5cc6ee;}
     .h{background:#fc7fc1;}
     .o2{background:#f5856e ;} 
-   #home_article #dpm-bxslider  .bx-wrapper  .bx-pager.bx-default-pager a{width:23px;height: 3px;}
-   #home_article #dpm-bxslider .bx-wrapper .bx-pager{left: 40%;}
-   #home_article #dpm-bxslider .bx-pager{bottom:-18px;}
-   #home_article #dpm-bxslider .bx-pager.bx-default-pager a.active{background: #17D7C2;}
-   #home_article  #dpm-bxslider .bxslider1 .bx-pager{bottom:-20px;}
+    #home_article #dpm-bxslider  .bx-wrapper  .bx-pager.bx-default-pager a{width:23px;height: 3px;}
+    #home_article #dpm-bxslider .bx-wrapper .bx-pager{left: 40%;}
+    #home_article #dpm-bxslider .bx-pager{bottom:-19px;}
+    #home_article #dpm-bxslider .bx-pager.bx-default-pager a.active{background: #17D7C2;}
     #home_article .shouwk{background: url('http://static.mingyizhudao.com/147219201837545') no-repeat;background-position:-96px 0;background-size: 200px 70px;}
     #home_article .guk{background: url('http://static.mingyizhudao.com/147219201837545') no-repeat;background-position:-128px 0;background-size: 200px 70px;}
     #home_article .shenjwk{background: url('http://static.mingyizhudao.com/147219201837545') no-repeat;background-position:-63px 0;background-size: 200px 70px;}
@@ -80,7 +79,7 @@ $SITE_6 = PatientStatLog::SITE_6;
             </div>
             <div class="pl10 pr10 bg-white mt20 pb10" id="dpm-bxslider">
                 <ul class="bxslider1">
-                    <li>
+             <!--        <li>
                       <div class="grid pt10">  
                      <div class="col-1 w33 pr10">  
                          <a class="departmentcontent" data-href="<?php echo $urlHospitalTop; ?>/innerDeptId/1/disease_sub_category/1" data-name="外科">  
@@ -163,7 +162,7 @@ $SITE_6 = PatientStatLog::SITE_6;
                          </a>  
                      </div>  
                  </div>    
-                </li>
+              </li>
                 <li>
                  <div class="grid pt10">  
                      <div class="col-1 w33 pr10">  
@@ -247,9 +246,10 @@ $SITE_6 = PatientStatLog::SITE_6;
                          </a>  
                      </div>  
                  </div>    
-                </li>
+                </li>  -->
             </ul>
             </div>
+
             <div class="text-right">
                 <div id="freePhone" href="javascript:;">
                     免费
@@ -407,7 +407,7 @@ $SITE_6 = PatientStatLog::SITE_6;
         $(' .bxslider').bxSlider({
             mode: 'fade',
             slideMargin: 0,
-            controls: true,
+            controls: false,
             auto: true
         });
         $('#home_article #team-bxslider .bxslider').click(function () {
@@ -416,9 +416,182 @@ $SITE_6 = PatientStatLog::SITE_6;
         });
 
         //轮播图2
-        
+        var text=
+       
+                    '<li class="slide">'+
+                      '<div class="grid pt10"> ' +
+                     '<div class="col-1 w33 pr10">  '+
+                        '<a class="departmentcontent" data-href="<?php echo $urlHospitalTop; ?>/innerDeptId/1/disease_sub_category/1" data-name="首外科"> '+ 
+                            '<div class="bg-blue4"> '+
+                                 '<div class="font-s16 pl5 pt5">'+  
+                                     '首外科 '+ 
+                                 '</div> '+ 
+                                 '<div class="grid pb5 pr5">' + 
+                                    ' <div class="col-1"></div> '+ 
+                                     '<div class="col-0 shouwk w35p h35p"></div>' + 
+                                 '</div> ' +
+                             '</div>  '+
+                        ' </a> '+ 
+                     '</div>' + 
+                     '<div class="col-1 w33 pl5 pr5">' + 
+                         '<a class="departmentcontent" data-href="<?php echo $urlHospitalTop; ?>/innerDeptId/2/disease_sub_category/13" data-name="骨科"> '+ 
+                            ' <div class="y1"> '+
+                                ' <div class="font-s16 pl5 pt5"> '+
+                                     '骨科 '+ 
+                                 '</div> ' +
+                                 '<div class="grid pb5 pr5"> ' +
+                                     '<div class="col-1"></div> ' +
+                                     '<div class="col-0 guk w35p h35p"></div> ' +
+                                 '</div>  '+
+                            '</div> '+ 
+                         '</a> ' +
+                     '</div> '+ 
+                     '<div class="col-1 w33 pl10">' + 
+                        '<a class="departmentcontent" data-href="<?php echo $urlHospitalTop; ?>/innerDeptId/3/disease_sub_category/18" data-name="神经外科">'+
+                             '<div class="o1" > '+
+                                 '<div class="font-s16 pl5 pt5"> '+ 
+                                    ' 神经外科 ' +
+                                 '</div>  '+
+                                 '<div class="grid pb5 pr5">  '+
+                                     '<div class="col-1"></div>  '+
+                                     '<div class="col-0 shenjwk w35p h35p"></div> '+ 
+                                 '</div>'+  
+                             '</div> ' +
+                         '</a>' + 
+                    '</div>' +
+                 '</div> ' +
+                 '<div class="grid mt15"> ' +
+                    '<div class="col-1 w33 pr10"> '+ 
+                         '<a class="departmentcontent" data-href="<?php echo $urlHospitalTop; ?>/innerDeptId/4/disease_sub_category/21" data-name="胸外科">  '+
+                             '<div class="b1"> '+
+                                 '<div class="font-s16 pl5 pt5"> '+
+                                    ' 胸外科'  +
+                                '</div> ' +
+                                '<div class="grid pb5 pr5"> ' +
+                                     '<div class="col-1"></div> ' +
+                                     '<div class="col-0 xiongwk w35p h35p"></div>'+
+                                ' </div>'  +
+                             '</div> ' +
+                         '</a> ' +
+                     '</div>'+  
+                     '<div class="col-1 w33 pl5 pr5">'+ 
+                        ' <a class="departmentcontent" data-href="<?php echo $urlHospitalTop; ?>/innerDeptId/5/disease_sub_category/28" data-name="泌尿外科"> '+ 
+                             '<div class="bg-blue5">' +
+                                 '<div class="font-s16 pl5 pt5"> '+ 
+                                     '泌尿外科 '+
+                                 '</div>'+  
+                                 '<div class="grid pb5 pr5">'+  
+                                    ' <div class="col-1"></div>'+  
+                                     '<div class="col-0 minwk w35p h35p"></div> '+ 
+                                 '</div>'+  
+                            '</div> '+ 
+                        '</a>'+  
+                     '</div> '+ 
+                     '<div class="col-1 w33 pl10">'+  
+                         '<a class="departmentcontent" data-name="眼科 " data-href="<?php echo $urlHospitalTop; ?>/innerDeptId/6/disease_sub_category/31">'+  
+                             '<div class="b2"> '+
+                                 '<div class="font-s16 pl5 pt5"> '+ 
+                                    '眼科  '+
+                                 '</div>  '+
+                                 '<div class="grid pb5 pr5">'  +
+                                     '<div class="col-1"></div>'  +
+                                     '<div class="col-0 yank w35p h35p"></div> ' +
+                                 '</div>  '+
+                             '</div>' + 
+                         '</a>  '+
+                     '</div> ' +
+                 '</div> '+   
+                '</li>'+
+                '<li class="slide">'+
+                 '<div class="grid pt10"> '+ 
+                     '<div class="col-0 w33 pr10">'+  
+                        '<a class="departmentcontent" data-href="<?php echo $urlHospitalTop; ?>/innerDeptId/1/disease_sub_category/1" data-name="心血管外科">'+  
+                            '<div class="y2" > '+
+                                ' <div class="font-s16 pl5 pt5"> '+ 
+                                     '心血管外科  '+
+                                 '</div> '+ 
+                                 '<div class="grid pb5 pr5">'+ 
+                                     '<div class="col-1"></div> '+ 
+                                     '<div class="col-0 xinxgwk w35p h35p"></div>'+ 
+                                 '</div> ' +
+                             '</div> '+
+                         '</a>'+  
+                     '</div>'+ 
+                     '<div class="col-0 w33 pl5 pr5"> '+ 
+                         '<a class="departmentcontent" data-href="<?php echo $urlHospitalTop; ?>/innerDeptId/2/disease_sub_category/13" data-name="耳鼻喉科"> '+ 
+                             '<div class="b3" >  '+
+                                ' <div class="font-s16 pl5 pt5"> '+ 
+                                    '耳鼻喉科 '+ 
+                                 '</div>'+  
+                                 '<div class="grid pb5 pr5"> '+ 
+                                     '<div class="col-1"></div> '+ 
+                                     '<div class="col-0 erbhk w35p h35p"></div>'+  
+                                ' </div> '+
+                             '</div> '+ 
+                         '</a> '+ 
+                     '</div>  '+
+                     '<div class="col-0 w33 pl10"> '+ 
+                        '<a class="departmentcontent" data-href="<?php echo $urlHospitalTop; ?>/innerDeptId/3/disease_sub_category/18" data-name="整形外科">'+  
+                             '<div class="h" > '+
+                               '<div class="font-s16 pl5 pt5">  '+
+                                    '整形外科 '+
+                                 '</div> '+
+                                 '<div class="grid pb5 pr5"> '+ 
+                                    '<div class="col-1"></div>  '+
+                                     '<div class="col-0 zhengxwk w35p h35p"></div>  '+
+                                 '</div> '+
+                            '</div>'+ 
+                         '</a> ' +
+                    '</div> '+
+                 '</div> '+ 
+                ' <div class="grid mt15"> '+ 
+                     '<div class="col-1 w33 pr10">' + 
+                         '<a class="departmentcontent" data-href="<?php echo $urlHospitalTop; ?>/innerDeptId/4/disease_sub_category/21" data-name="颌面外科"> '+ 
+                            ' <div class="b4" >  '+
+                                '<div class="font-s16 pl5 pt5"> '+ 
+                                   ' 颌面外科  '+
+                                 '</div> '+
+                                 '<div class="grid pb5 pr5">'+  
+                                    '<div class="col-1"></div>  '+
+                                    '<div class="col-0 hemwk w35p h35p"></div> '+
+                                 '</div>  '+
+                             '</div>'+  
+                         '</a> '+ 
+                     '</div>'+  
+                     '<div class="col-1 w33 pl5 pr5">'+  
+                         '<a class="departmentcontent" data-href="<?php echo $urlHospitalTop; ?>/innerDeptId/5/disease_sub_category/28" data-name="妇科">'+  
+                             '<div class="o2" > '+
+                                 '<div class="font-s16 pl5 pt5"> ' +
+                                     '妇科 '+
+                                ' </div> ' +
+                                 '<div class="grid pb5 pr5">'+  
+                                    ' <div class="col-1"></div> '+ 
+                                    ' <div class="col-0 fuk w35p h35p"></div> '+ 
+                                 '</div> ' +
+                             '</div> '+
+                         '</a> ' +
+                     '</div>' + 
+                     '<div class="col-1 w33 pl10">' + 
+                        ' <a class="departmentcontent" data-name="小儿外科" data-href="<?php echo $urlHospitalTop; ?>/innerDeptId/6/disease_sub_category/31"> '+ 
+                            ' <div  class="bg-orange"> '+ 
+                                ' <div class="font-s16 pl5 pt5"> ' +
+                                   '小儿外科 '+ 
+                                ' </div>'+
+                                 '<div class="grid pb5 pr5"> '+ 
+                                     '<div class="col-1"></div> '+
+                                     '<div class="col-0 xiaoewk w35p h35p"></div> ' +
+                                 '</div> '+ 
+                             '</div>' + 
+                         '</a> '+
+                     '</div> ' +
+                 '</div> '   +
+                '</li>';
+          
+            
+         $('#home_article #dpm-bxslider .bxslider1').html(text);
 
-         $('#dpm-bxslider .bxslider1').bxSlider({
+
+         $(' .bxslider1').bxSlider({
             mode: 'fade',
             slideMargin: 0,
             controls: false,
