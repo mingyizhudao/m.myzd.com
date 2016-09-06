dd585c50cb9bc4db346ad688b6911a53c2705831
crackbest <147566179@qq.com>
2016-09-06 17:35:42 +0800

    增加登录和30分钟设置，多次登录产生多个token

--- protected/models/app/UserTokenMongo.php
+++ protected/models/app/UserTokenMongo.php
@@ -14,8 +14,14 @@ class UserTokenMongo extends EMongoDocument
 {
     public $id;
     public $user_id;
+    public $username;
     public $token;
     public $time_expiry;
+    const EXPIRY_DEFAULT = 1800;    //30 minutes
+    const ERROR_NONE = 0;
+    const ERROR_NOT_FOUND = 1;
+    const ERROR_INACTIVE = 2;
+    const ERROR_EXPIRED = 3;
     // This has to be defined in every model, this is same as with standard Yii ActiveRecord
     public static function model($className=__CLASS__)
     {
@@ -32,11 +38,12 @@ class UserTokenMongo extends EMongoDocument
 	// will receive user inputs.
 	return array(
             array('user_id', 'numerical', 'integerOnly'=>true),
-            array('token', 'length', 'max'=>32),
+            array('token', 'length', 'is' => 32),
             array('time_expiry', 'length', 'max'=>11),
+            array('username', 'length', 'max'=>128),
             // The following rule is used by search().
             // @todo Please remove those attributes that should not be searched.
-            array('id,user_id,token,time_expiry', 'safe', 'on'=>'search'),
+            array('id,user_id,token,time_expiry,username', 'safe', 'on'=>'search'),
 	);
     }
     //直接保存
@@ -50,7 +57,54 @@ class UserTokenMongo extends EMongoDocument
         $this->time_expiry=time();
         $this->save();
     }
-    
+    // 创建 token。
+    public function createTokenPatient($userId, $username, $userHostIp, $userMacAddress) {
+        return $this->createToken($userId, $username, StatCode::USER_ROLE_PATIENT, $userHostIp, $userMacAddress);
+    }
+    //实际创建TOKE
+    public function createToken($userId, $username, $userRole, $userHostIp, $userMacAddress) {
+        $this->setUserId($userId);
+        $this->setUsername($username);
+        $this->setToken();
+        $this->setTimeExpiry();
+    }
+     /*     * ****** Accessors ******* */
+
+
+
+    public function getToken() {
+        return $this->token;
+    }
+
+    private function setToken() {
+        $this->token = strtoupper(substr(str_shuffle(MD5(microtime())), 0, 32));   // refer to helper.php
+    }
+
+    public function getUserId() {
+        return $this->user_id;
+    }
+
+    private function setUserId($v) {
+        $this->user_id = $v;
+    }
+
+    public function getUsername() {
+        return $this->username;
+    }
+
+    private function setUsername($v) {
+        $this->username = $v;
+    }
+
+    public function getTimeExpiry() {
+        return $this->time_expiry;
+    }
+
+    private function setTimeExpiry() {
+        $duration = self::EXPIRY_DEFAULT;
+        $now = time();
+        $this->time_expiry = $now + $duration;
+    }
     
     
 }
