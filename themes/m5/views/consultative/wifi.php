58940230c32e99aa67e5e8cfcad91a4381e39a90
popoAOAOgroan <329565409@qq.com>
2016-11-01 15:23:43 +0800

    更新wifi落地页

--- themes/m5/views/consultative/wifi.php
+++ themes/m5/views/consultative/wifi.php
@@ -9,6 +9,29 @@ $urlGetSmsVerifyCode = $this->createAbsoluteUrl('/api/smsverifycode');
 $urlGetBooking = $this->createAbsoluteUrl('/api/quickbooking');
 ?>
 <style type="text/css">
+    .wifi-contain{
+        display: box;             /* OLD - Android 4.4- */
+        display: -webkit-box;     /* OLD - iOS 6-, Safari 3.1-6 */
+        display: -moz-box;       /* OLD - Firefox 19- (buggy but mostly works) */
+        display: -ms-flexbox;     /* TWEENER - IE 10 */
+        display: -webkit-flex;   /* NEW - Chrome */
+        display: flex;           /* NEW, Spec - Opera 12.1, Firefox 20+ */
+        /* 12版 */
+        -webkit-flex-direction: column;
+        -moz-flex-direction: column;
+        -ms-flex-direction: column;
+        -o-flex-direction: column;
+        flex-direction: column;
+        height: 100%;
+    }
+    .mid-contain{
+        -webkit-box-flex: 1;
+        -moz-box-flex: 1;
+        -webkit-flex: 1;          /* Chrome */
+        -ms-flex: 1;              /* IE 10 */
+        flex: 1;        
+        overflow-y: scroll;
+    }
     .y-color{
         background: rgba(239,202,36,1);
     }
@@ -42,7 +65,7 @@ $urlGetBooking = $this->createAbsoluteUrl('/api/quickbooking');
         border-radius: 5px;
         color: #000;
     }
-    h2,h3{
+    h3,h4{
         font-weight: bold;
     }
     .w-b-img{
@@ -159,8 +182,9 @@ $urlGetBooking = $this->createAbsoluteUrl('/api/quickbooking');
         text-align: center;
         padding: 0 10px;
     }
-    .w-b-4 img{
+    .w-b-4 .w-b-4-2l img{
         margin: 20px 0;
+        height: 100px;
     }
     .w-b-4-2l >div{
         float: left;
@@ -219,12 +243,13 @@ $urlGetBooking = $this->createAbsoluteUrl('/api/quickbooking');
     }
 
     .w-b-7{
-        margin: 50px 0;
+        margin: 50px 0 10px 0;
     }
 
     .w-b-8{
         padding: 10px 20px;
         background-color: #858585;
+        height: 60px;
     }
     .w-b-8 button{
         background-color: #19AFA6;
@@ -240,95 +265,99 @@ $urlGetBooking = $this->createAbsoluteUrl('/api/quickbooking');
     }
 </style>
 <article class="active" data-scroll="true" class="wifi-page">
-    <div class="w-b-1">
-        <p class="d-color-t f-size-l"><i class="w-b-logo"></i>做手术就找名医主刀</p>
-        <div>
-            <h2 class="w-color-t">想找北上广名医动手术？</h2>
-            <p class="w-color-t"><span class="l-color-t">3万</span>三甲主刀名医帮您约，<span class="l-color-t">不用等床位</span>！</p>
-        </div>
-        <button class="w-btn freePhone">电话快速预约 ></button>
-    </div>
-    <div class="w-b-2">
-        <h3 class="d-color-t">直约全国各大名院主刀医生来做手术</h3>
-        <p>手术档期、床位安排、主刀医生，<span class="g-color-t">一个电话</span>统统搞定！</p>
-        <img class="w-b-2" src="http://static.mingyizhudao.com/147763500968412">
-        <ul>
-            <li><span class="g-color">1</span>中山大学附属肿瘤医院/中山医院/南方医院</li>
-            <li><span class="g-color">2</span>湘雅医院</li>
-            <li><span class="g-color">3</span>长征医院/长海医院/华山医院/瑞金医院</li>
-            <li><span class="g-color">4</span>南京军区总院</li>
-            <li><span class="g-color">5</span>积水潭医院/解放军总院/协和医院/天坛医院</li>
-        </ul>
-    </div>
-    <div class="w-b-3">
-        <div>
-            挑选<span class="l-color-t">更多</span>名院医生，尽在<span class="l-color-t">名医主刀</span>！
-        </div>
-        <form>
-            <ul>
-                <li><span>姓名：</span><input id="booking_name" placeholder="请输入您的姓名" type="" name=""></li>
-                <li><span>电话：</span><input id="booking_mobile" placeholder="请输入您的电话" type="" name=""></li>
-                <!-- <li><span>所在城市：</span><input placeholder="请选择您所在的城市" type="" name=""></li> -->
-                <li>
-                    <span>验证码：</span>
+    <div class="wifi-contain">
+        <div class="mid-contain">
+            <div class="w-b-1">
+                <p class="d-color-t f-size-l"><i class="w-b-logo"></i>做手术就找名医主刀</p>
+                <div>
+                    <h3 class="w-color-t">想找北上广名医动手术？</h3>
+                    <p class="w-color-t"><span class="l-color-t">3万</span>三甲主刀名医帮您约，<span class="l-color-t">不用等床位</span>！</p>
+                </div>
+                <button class="w-btn freePhone">电话快速预约 ></button>
+            </div>
+            <div class="w-b-2">
+                <h4 class="d-color-t">直约全国知名主刀医生做手术</h4>
+                <p>手术档期、床位安排，<span class="g-color-t">一站搞定</span>！</p>
+                <img class="w-b-2" src="http://static.mingyizhudao.com/147763500968412">
+                <ul>
+                    <li><span class="g-color">1</span>中山大学附属肿瘤医院/中山医院/南方医院</li>
+                    <li><span class="g-color">2</span>湘雅医院</li>
+                    <li><span class="g-color">3</span>长征医院/长海医院/华山医院/瑞金医院</li>
+                    <li><span class="g-color">4</span>南京军区总院</li>
+                    <li><span class="g-color">5</span>积水潭医院/解放军总院/协和医院/天坛医院</li>
+                </ul>
+            </div>
+            <div class="w-b-3">
+                <div>
+                    挑选<span class="l-color-t">更多</span>名院医生，尽在<span class="l-color-t">名医主刀</span>！
+                </div>
+                <form>
+                    <ul>
+                        <li><span>姓名：</span><input id="booking_name" placeholder="请输入您的姓名" type="text" name=""></li>
+                        <li><span>电话：</span><input id="booking_mobile" placeholder="请输入您的电话" type="tel" name=""></li>
+                        <!-- <li><span>所在城市：</span><input placeholder="请选择您所在的城市" type="" name=""></li> -->
+                        <li>
+                            <span>验证码：</span>
+                            <div>
+                                <input id="booking_code" placeholder="请输入验证码" type="number" name="">
+                                <button id="btn-sendSmsCode">验证码</button>
+                            </div>
+                        </li>
+                        <li><span>预约医生：</span><input id="booking_doctor" placeholder="请输入医生名字，没有可不填" type="text" name=""></li>
+                    </ul>
+                    <p>
+                        <button id="btn-booking" class="w-btn-rx">提交预约</button>
+                    </p>
+                </form>
+            </div>
+            <div class="w-b-4">
+                <h4>别让等待延误了生命健康</h4>
+                <p><span class="g-color-t">12h内</span>确认面诊时间，<span class="g-color-t">7天内</span>筹备住院手术</p>
+                <img src="http://static.mingyizhudao.com/147764030179738">
+                <div class="w-b-4-2l">
                     <div>
-                        <input id="booking_code" placeholder="请输入验证码" type="" name="">
-                        <button id="btn-sendSmsCode">验证码</button>
+                        <img src="http://static.mingyizhudao.com/147764095578977">
+                        <p>无须漫长苦等主刀医生</p>
+                        <p class="g-color-t">延误救治</p>
                     </div>
-                </li>
-                <li><span>预约医生：</span><input id="booking_doctor" placeholder="请输入您想预约的主刀医生，没有可不填" type="" name=""></li>
-            </ul>
-            <p>
-                <button id="btn-booking" class="w-btn-rx">提交预约</button>
-            </p>
-        </form>
-    </div>
-    <div class="w-b-4">
-        <h3>别让等待延误了生命健康</h3>
-        <p><span class="g-color-t">12小时内</span>确认面诊时间，<span class="g-color-t">7天内</span>安排住院准备手术</p>
-        <img src="http://static.mingyizhudao.com/147764030179738">
-        <div class="w-b-4-2l">
-            <div>
-                <img src="http://static.mingyizhudao.com/147764095578977">
-                <p>无须漫长苦等主刀医生</p>
-                <p class="g-color-t">延误救治</p>
+                    <div>
+                        <img src="http://static.mingyizhudao.com/147764095603274">
+                        <p>无须担心医院没有床位</p>
+                        <p class="g-color-t">危及生命</p>
+                    </div>
+                </div>
             </div>
-            <div>
-                <img src="http://static.mingyizhudao.com/147764095603274">
-                <p>无须担心医院没有床位</p>
-                <p class="g-color-t">危及生命</p>
+            <div class="w-b-5">
+                <p>做手术找名医主刀，就是<span class="l-color-t">快</span>！</p>
+                <button class="w-btn online-adv">在线咨询预约</button>
+            </div>
+            <div class="w-b-6">
+                <h4>帮助一个人，幸福一家人</h4>
+                <p>成立至今帮助<span class="g-color-t">数万患者</span>及时获得手术</p>
+                <div>
+                    <p>案例一 是什么夺去了11岁小女孩奔跑的权利？</p>
+                    <video class="w6video" x-webkit-airplay="true" webkit-playsinline="" playsinline="true" preload="none" poster="http://static.mingyizhudao.com/147764536727257" src="http://222.73.3.72/vhot2.qqvideo.tc.qq.com/l0326dsrvqi.m701.mp4?vkey=495E22F1C26CF0DF6FCFF315D3AA4E304D037E9F059D1192AAA866A210DD5701AC0E77782B025EFF3A005CFA886F7C28AFD7B359C5BD8D85B49D28854F8B86A6A9B4B56F1D10B5973BE952D2CA3B45BB314EDEE6BF4E6777&br=29&platform=2&fmt=auto&level=0&sdtfrom=v3010&guid=98bc1931e016862d615374312bc8ff22"></video>
+                </div>
+                <div>
+                    <p>案例二 扭曲的人生：名医主刀还我18年正直梦！（上）</p>
+                    <video class="w6video" x-webkit-airplay="true" webkit-playsinline="" playsinline="true" preload="none" poster="http://static.mingyizhudao.com/147764536308615" src="http://222.73.132.153/vhot2.qqvideo.tc.qq.com/y0332exruz4.m701.mp4?vkey=98396AAC66D6A100A0E3A11FF6E67B5AE71CACD0FB3EEEE9F1A1ED4F56696DDAB35E29551CE5C22CF6340AF36EA35163939ED6884EEA1E1106288CB930B44C5AA933386B5EA555FBD39813DAB1F142F55C69A723B105CB1F&br=29&platform=2&fmt=auto&level=0&sdtfrom=v3010&guid=98bc1931e016862d615374312bc8ff22"></video>
+                </div>
+                <div class="w-6-link">
+                    <i></i>
+                    <a class="g-color-t" href="http://m.mingyizhudao.com/mobile/event/view-page-life.html">同病不同命：名医主刀助肺癌陈先生重获生命</a>
+                </div>
+                <div class="w-6-link">
+                    <i></i>
+                    <a class="g-color-t" href="http://m.mingyizhudao.com/mobile/event/view-page-lifeExpect.html">患者心声：活下去的希望是名医主刀给的</a>
+                </div>
+            </div>
+            <div class="w-b-7">
+                <img class="online-adv" src="http://static.mingyizhudao.com/147764327385872">
             </div>
         </div>
-    </div>
-    <div class="w-b-5">
-        <p>做手术找名医主刀，就是<span class="l-color-t">快</span>！</p>
-        <button class="w-btn online-adv">在线咨询预约</button>
-    </div>
-    <div class="w-b-6">
-        <h3>帮助一位患者，幸福整个家庭</h3>
-        <p>成立至今帮助<span class="g-color-t">数万患者</span>及时获得手术</p>
-        <div>
-            <p>案例一 是什么夺去了11岁小女孩奔跑的权利？</p>
-            <video class="w6video" x-webkit-airplay="true" webkit-playsinline="" playsinline="true" preload="none" poster="http://static.mingyizhudao.com/147764536727257" src="http://222.73.3.72/vhot2.qqvideo.tc.qq.com/l0326dsrvqi.m701.mp4?vkey=495E22F1C26CF0DF6FCFF315D3AA4E304D037E9F059D1192AAA866A210DD5701AC0E77782B025EFF3A005CFA886F7C28AFD7B359C5BD8D85B49D28854F8B86A6A9B4B56F1D10B5973BE952D2CA3B45BB314EDEE6BF4E6777&br=29&platform=2&fmt=auto&level=0&sdtfrom=v3010&guid=98bc1931e016862d615374312bc8ff22"></video>
+        <div class="w-b-8">
+            <button class="freePhone"><i></i>电话预约三甲主刀名医</button>
         </div>
-        <div>
-            <p>案例二 扭曲的人生：名医主刀还我18年正直梦！（上）</p>
-            <video class="w6video" x-webkit-airplay="true" webkit-playsinline="" playsinline="true" preload="none" poster="http://static.mingyizhudao.com/147764536308615" src="http://222.73.132.153/vhot2.qqvideo.tc.qq.com/y0332exruz4.m701.mp4?vkey=98396AAC66D6A100A0E3A11FF6E67B5AE71CACD0FB3EEEE9F1A1ED4F56696DDAB35E29551CE5C22CF6340AF36EA35163939ED6884EEA1E1106288CB930B44C5AA933386B5EA555FBD39813DAB1F142F55C69A723B105CB1F&br=29&platform=2&fmt=auto&level=0&sdtfrom=v3010&guid=98bc1931e016862d615374312bc8ff22"></video>
-        </div>
-        <div class="w-6-link">
-            <i></i>
-            <a class="g-color-t" href="http://m.mingyizhudao.com/mobile/event/view-page-life.html">同病不同命：名医主刀助肺癌陈先生重获生命</a>
-        </div>
-        <div class="w-6-link">
-            <i></i>
-            <a class="g-color-t" href="http://m.mingyizhudao.com/mobile/event/view-page-lifeExpect.html">患者心声：活下去的希望是名医主刀给的</a>
-        </div>
-    </div>
-    <div class="w-b-7">
-        <img class="online-adv" src="http://static.mingyizhudao.com/147764327385872">
-    </div>
-    <div class="w-b-8">
-        <button class="freePhone"><i></i>电话预约三甲主刀名医</button>
     </div>
 </article>
 <script>
@@ -349,7 +378,8 @@ $urlGetBooking = $this->createAbsoluteUrl('/api/quickbooking');
             });
         });
         $('.online-adv').click(function(){
-            location.href = 'http://dct.zoosnet.net/LR/Chatpre.aspx?id=DCT73779034&lng=cn';
+            window.open('http://dct.zoosnet.net/LR/Chatpre.aspx?id=DCT73779034&lng=cn', '_blank');
+            // location.href = 'http://dct.zoosnet.net/LR/Chatpre.aspx?id=DCT73779034&lng=cn';
         });
         $('.w6video').click(function(){
 
