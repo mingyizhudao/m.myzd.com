19a011cd10f42cc5265b8251ffc8468c9820b65c
popoAOAOgroan <329565409@qq.com>
2016-11-01 13:45:23 +0800

    删除js css img

--- themes/vendor/js/baiduStatistics.js
+++
@@ -1,7 +0,0 @@
-var _hmt = _hmt || [];
-(function () {
-    var hm = document.createElement("script");
-    hm.src = "//hm.baidu.com/hm.js?1ce45a3053a6aeafe8331f39e8beb121";
-    var s = document.getElementsByTagName("script")[0];
-    s.parentNode.insertBefore(hm, s);
-})();
