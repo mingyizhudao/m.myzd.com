92729d2576b5f3bdef7a626715120aa31758eca0
crackbest <147566179@qq.com>
2016-11-01 11:37:35 +0800

    添加落地页位置

--- protected/modules/mobile/controllers/ConsultativeController.php
+++ protected/modules/mobile/controllers/ConsultativeController.php
@@ -1,6 +1,10 @@
 <?php
 class ConsultativeController extends MobileController {
     public function actionIndex() {
+        $this->render('index');
+    }
+    //wifi落地页
+     public function actionWifi() {
         $this->render('wifi');
     }
 }
